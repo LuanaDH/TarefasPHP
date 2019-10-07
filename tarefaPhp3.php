4051a62ba3f93b2b7fa3183f06a0e22f8a390462
LuanaDH <solimenolua@gmail.com>
2019-10-07 00:12:11 -0300

    Feito até Ex 3

---
+++ tarefaPhp3.php
@@ -0,0 +1,28 @@
+<?php 
+//EXERCICIO 1
+/*
+for($i=0; $i<=100;$i++){
+    echo $i." ";
+}*/
+
+//EXERCICIO 2 
+/*
+echo mt_rand(1, 100);
+$varEx2= 11;
+echo "O número aleatório gerado foi $varEx2 <br>";
+for($i=0; $i<=$varEx2; $i++){
+    echo $i." ";
+}*/
+
+//EXERCICIO 3  
+/*
+echo "Tabuada do 2 <br>";
+for($i=0; $i<=10; $i++){
+    $multiplicar= $i*2;
+    echo $multiplicar." ";
+}*/
+
+//EXERCICIO 4 
+
+
+?>
