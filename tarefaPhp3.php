4d30151e7222b13c04db5bf3a9ca91e9e53f9e3d
LuanaDH <solimenolua@gmail.com>
2019-10-07 23:16:34 -0300

    falta apenas ex.13

--- tarefaPhp3.php
+++ tarefaPhp3.php
@@ -80,7 +80,111 @@ echo "Foram lançadas $numJogadas vezes a moeda"*/
     } while ($i < count($arrayEx6));*/
 
 //EXERCICIO 7
-
+/*
 $numeroAleatorio= [];
- 
+    for($i = 0; $i <= 10; $i++){
+        $numeroAleatorio [] = mt_rand (0, 10);
+    }
+    var_dump($numeroAleatorio);*/
+//FOR
+/*
+    for($i = 0; $i < count($numeroAleatorio); $i++){
+        if($numeroAleatorio[$i] == 5){
+            break;
+        }
+        echo "Encontramos um numero 5!";
+            break;
+    }*/
+
+    //WHILE
+/*
+    $i = 0;
+    while($i < count($numeroAleatorio)){
+        if($numeroAleatorio[$i] == 5){
+            break;
+        }
+        echo "Encontramos o número 5!";
+            break;
+    }*/
+
+    //DO WHILE
+/*    
+    $i = 0;
+    do{
+        echo "Encontramos o número 5!";
+        break;
+        $i++;
+    } while($i < count($numeroAleatorio));*/
+        
+//EXERCICIO 8
+/*    
+    foreach (range('a','n') as $index => $value){
+        echo "Na posição $index, está o valor $value <br>";
+    }*/
+
+//EXERCICIO 9 
+/*
+    $mascote = ["animal"=>"cachorro", "idade"=>5, "altura"=>"0,60", "nome"=>"Cookie"];
+    foreach($mascote as $respostas => $mascote){
+        echo "$respostas: $mascote"."<br>";
+    }*/
+
+//EXERCICIO 11
+/*
+$ceu = ["Itália"=>"Roma", "Luxemburgo"=>"Luxemburgo",
+"Bélgica"=> "Bruxelas", "Dinamarca"=>"Copenhagen",
+"Finlândia"=>"Helsinki", "França" => "Paris",
+"Eslováquia"=>"Bratislava", "Eslovênia"=>"Liubliana",
+"Alemanha" => "Berlim", "Grécia" => "Atenas",
+"Irlanda"=>"Dublin", "Holanda"=>"Amsterdã",
+"Portugal"=>"Lisboa", "Espanha"=>"Madri",
+"Suécia"=>"Estocolmo", "Reino Unido"=>"Londres",
+"Chipre"=>"Nicósia", "Lituânia"=>"Vilnius", "República
+Tcheca"=>"Praga", "Estônia"=>"Tallinn",
+"Hungria"=>"Budapeste","Letônia"=>"Riga", "Malta"=>"Valletta",
+"Áustria" => "Viena", "Polônia"=>"Varsóvia"];
+    foreach($ceu as $capitais => $ceu){
+        echo "A capital da $capitais é $ceu.<br>";
+    }*/
+
+//EXERCICIO 12
+/*
+$ceu = [
+    "Argentina" => ["Buenos Aires", "Córdoba", "Santa Fé"],
+    "Brasil" => ["Brasília", "Rio de Janeiro", "São Paulo"],
+    "Colômbia" => ["Cartagena", "Bogotá", "Barranquilla"],
+    "França" => ["Paris", "Nantes", "Lyon"],
+    "Itália" => ["Roma", "Milão", "Veneza"],
+    "Alemanha" => ["Munique", "Berlim", "Frankfurt"]
+    ];
+    foreach($ceu as $pais => $cidades){
+        echo "As cidades da $pais são: <br>";
+    foreach($cidades as $var => $cidades){
+        echo "-".$cidades."<br>";
+    }
+    }*/
+
+//EXERCICIO 13
+$ceu = [
+    ["Argentina" => ["Buenos Aires", "Córdoba", "Santa Fé"],"naAmerica" => True],
+
+    ["Brasil" => ["Brasília", "Rio de Janeiro", "São Paulo"], "naAmerica" => False],
+    //"Colômbia" => ["Cartagena", "Bogotá", "Barranquilla"],
+    //"França" => ["Paris", "Nantes", "Lyon"],
+    //"Itália" => ["Roma", "Milão", "Veneza"],
+    //"Alemanha" => ["Munique", "Berlim", "Frankfurt"]
+    ];
+
+    foreach($ceu as $pais => $cidades){
+        if($pais["naAmerica"] = True){
+            foreach($pais as $pais2){
+                echo "As cidades da $pais são: <br>";
+              foreach($pais[0] as $cidades){
+                                
+              }
+            }
+        }
+    }
+
+    
 ?>
